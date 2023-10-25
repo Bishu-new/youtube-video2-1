601d3956e4ce82738b0a18eff7d3c48383daa441
Bishu-new <bishu.sharma@outlook.com>
2023-10-25 07:51:29 +0530

    new added file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from flask import Flask , render_template , request , jsonify
+from flask import Flask , render_template , request ,redirect,url_for, jsonify
 from flask_cors import CORS , cross_origin
 import requests
 from bs4 import BeautifulSoup
@@ -16,70 +16,58 @@ def homepage ():
 @app.route("/videos", methods = ["POST" , "GET"])
 def index():
     if request.method == 'POST':
-        
+        title1 = []
         try:
             searchString = request.form['content'].replace(" " , "_")
             Youtube_Url = 'https://www.youtube.com/c/@PW-Foundation/' + searchString
-            driver = webdriver.Chrome()
-            for url in Youtube_Url:  
-                driver.get('https://www.youtube.com/@PW-Foundation/' + searchString)
-                content = driver.page_source.encode("utf-8").strip()
-                soup = BeautifulSoup(content , 'html.parser')
-                titles = soup.findAll('yt-formatted-string' , id = 'video-title')
-                views = soup.findAll('span' , class_ = 'inline-metadata-item style-scope ytd-video-meta-block')
-                Videos_urls = soup.findAll('a' , id = 'video-title-link' , class_= 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media')
-                print('Channel:{}'.format(url))
-                i = 0 #views and time
-                j = 0 # for urls
-                with open('my_csv_file.csv' , 'w' , newline='') as csvfile:
-                 wirter = csv.writer(csvfile)
-                 wirter.writerow(['titles' , 'views' , 'time' , 'Video_link'])
-                 for video in url:
-                     wirter.writerow([video[titles] , video[views], video[views], video[Videos_urls]])
-                 
-
-
-                
-                
-                
-                 
-                for title in titles:
-                  print('\n{}\t{}\t{}\thttps:\\www.youtube.com{}'.format(title.text , views[i].text,views[i+1].text,Videos_urls[j].get('href')))
-
-                  i+=2
-                  j+=1
-
-
-            
-              # Initialize an empty list to store dictionaries
-                videos = []
+            driver = webdriver.Chrome()  
+            driver.get('https://www.youtube.com/@PW-Foundation/' + searchString)
+            content = driver.page_source.encode("utf-8").strip()
+            soup = BeautifulSoup(content , 'html.parser')
+            titles = soup.findAll('yt-formatted-string' , id = 'video-title')
+            views = soup.findAll('span' , class_ = 'inline-metadata-item style-scope ytd-video-meta-block')
+            Videos_urls = soup.findAll('a' , id = 'video-title-link' , class_= 'yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media')
+            print('Channel:{}'.format(Youtube_Url))
+            i = 0 #views and time
+            j = 0 # for urls
+               # Initialize an empty list to store dictionaries
+            for title, view, video_url in list(zip(titles, views, Videos_urls))[:6]:
+                video_info_text = f"{title.text} || {views[i].text} views {views[i + 1].text} https://www.youtube.com/{video_url['href']}"
                 mydict = {
-                "Product": searchString,
-                 "Title": titles,
-                 "Views": views,
-                 "Time": views,
-                 "video_link": Videos_urls
+                
+                 "Text": video_info_text,
+                    "video_link": "https://www.youtube.com/" + video_url['href']
                  }
-                videos.append(mydict)
-            print(url)    
-# Log the result
-            logging.info("Logged videos: %s", videos)
+                title1.append(mydict)
+                i+=2
+                j+=1
+            print(title1)
 
+
+            with open('video_data.csv' , 'w' , newline='' , encoding='utf-8') as csvfile:
+                            wirter = csv.writer(csvfile)
+                            wirter.writerow(['heading' , 'Views' , 'Time' , 'Link'])
+                            for item in title1:
+                                      wirter.writerow([item['Text'] , item["video_link"]])
+# Log the result
+            logging.info("Logged title: %s", title1)
+            return render_template('results.html' , title1 = title1)
         except Exception as e:
             print("exception while creating dictonary" , e)
-
             
-
-
-            return render_template('results.html' , videos = videos(len(videos)))
+            return redirect(url_for('homepage'))
+        
+            
+            
         except Exception as e :
             logging.info(e)
             return 'Something is wrong'
+            
         traceback.print_exc()
-        
+        return redirect(url_for('homepage'))    
 
     else : 
-        return render_template('index.html')    
+     return render_template('index.html')    
     
     
     
